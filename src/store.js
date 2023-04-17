bc6f4865ca8e1ec562b402bb8f4ed219a09ec4d9
alessiaBer <alexdeyn@gmail.com>
2023-04-17 16:59:00 +0200

    [add] fetchCard function to store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { reactive } from 'vue'
+import axios from 'axios'
 
 export const store = reactive({
-  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0'
+  loading: true,
+  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0',
+  cardName: null,
+  cardArchetype: null,
+  fetchCard() {
+    axios.get(this.YUGI_API)
+    .then(response => {
+      this.cardName = response.data.name
+      this.cardArchetype = response.data.archetype
+    })
+  }
 })
