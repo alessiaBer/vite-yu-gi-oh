01ca0ca148335aa1bf8936e385d0c19476b08fe8
alessiaBer <alexdeyn@gmail.com>
2023-04-17 16:38:55 +0200

    [add] components stylesheets

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,16 +1,23 @@
 <script>
 import CardItem from './CardItem.vue'
 export default {
-    name: CardsList,
+    name: 'CardsList',
     components: {
         CardItem
     }
 };
 </script>
 <template>
-  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-    <CardItem />
+  <div class="cardlist_container">
+    <div class="found">
+      <span>Found .. cards</span>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+      <CardItem />
+    </div>
   </div>
+  
+    
 </template>
 
 <style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  YUGI_API: ''
+  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0'
 })
