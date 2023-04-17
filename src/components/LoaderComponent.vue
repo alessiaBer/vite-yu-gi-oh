83bb4c7deef8f0be55323a9fdcfc529846fc72e1
alessiaBer <alexdeyn@gmail.com>
2023-04-17 18:14:54 +0200

    [change] loading gif

--- src/components/LoaderComponent.vue
+++ src/components/LoaderComponent.vue
@@ -5,10 +5,17 @@ export default {
 </script>
 <template>
     <div class="loader d-flex flex-column align-items-center justify-content-center">
-       <img class="rounded-circle" height="120" src="https://thumbs.gfycat.com/HardScientificFlee-max-1mb.gif" alt="Loading Gif">
+       <img class="rounded-circle" height="200" src="https://i.pinimg.com/originals/78/56/0f/78560f1998097c570eafc8cf9e30f8a3.gif" alt="Loading Gif">
         <span>Loading..</span>  
     </div>
     
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.loader {
+    img {
+        object-fit: cover;
+        aspect-ratio: 1 / 1;
+    }
+}
+</style>
